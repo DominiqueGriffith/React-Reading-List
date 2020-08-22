7ff778ae4aecec9a83a99e0c4d3fe0804915633a
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-08-22 12:57:53 -0400

    Updates

---
+++ precache-manifest.f7a7a3ce3dd0d022a3f53e394bd8f9e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c48284dbcc33c6857c7750c1d4cc14d",
+    "url": "/React-Reading-List/index.html"
+  },
+  {
+    "revision": "b3381682ec5ce293b73a",
+    "url": "/React-Reading-List/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "78f328a458a64edb01c1",
+    "url": "/React-Reading-List/static/js/2.373313d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Reading-List/static/js/2.373313d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3381682ec5ce293b73a",
+    "url": "/React-Reading-List/static/js/main.d7da35b1.chunk.js"
+  },
+  {
+    "revision": "0787b8286875b9624bc4",
+    "url": "/React-Reading-List/static/js/runtime-main.a8450ffd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-Reading-List/static/media/logo.ee7cd8ed.svg"
+  }
+]);
